8433d807627e7a3d19b74daed7ccd7319ea2581a
Madhu <madhusudhan.bhaskar@gmail.com>
2018-11-20 19:53:40 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,134 +1,172 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-import 'dart:async';
-import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
-import 'ui/animation.example.dart';
-
-class StaggerAnimation{
-  StaggerAnimation(this.controller)
-  : animation = Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
-        parent: controller,
-        curve: Interval(
-          0.0, 0.4,
-          curve: Curves.bounceIn,
-        ),
-      ),
-    ),
-    _opacity = new Tween(begin: 0.0, end: 0.4).animate(
-          new CurvedAnimation(
-            parent: controller,
-            curve: new Interval(
-              0.80,1.0,
-              curve: Curves.bounceOut,
-            ),
-          ),
-    ),
-    
-    color = ColorTween(
-      begin: Colors.indigo[100],
-      end: Colors.orange[800],
-    ).animate(
-      CurvedAnimation(
-        parent: controller,
-        curve: Interval(
-          0.80, 1.0,
-          curve: Curves.fastOutSlowIn,
-        ),
-      ),
-    );
-  final AnimationController controller;
-  Animation<double> animation;
-  Animation<double> _opacity;
-  Animation<Color> color;
-} 
-class SplashScreen extends StatelessWidget {
-  SplashScreen({@required AnimationController controller})
-  :animation = new StaggerAnimation(controller);
-
-  final StaggerAnimation animation;
-
-  @override
-  Widget build(BuildContext context) {
-    return  Scaffold (
-      body:  Container (
-        child: Column ( 
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-              new Center(
-                child: Container(
-                  margin: EdgeInsets.symmetric(vertical: 10.0),
-                  height: animation.animation.value*100,
-                  width: animation.animation.value*100,
-                  child: FlutterLogo(),
-                ), 
-              ),
-              new Opacity(
-                opacity: animation._opacity.value,
-                child: CircularProgressIndicator(
-                  valueColor: animation.color,
-                ),
-              ),
-          ],
-        ),
-      ),
-    );
-  }
+import 'package:flutter/services.dart';
+import 'package:binge/pages/main.search.page.dart';
+import 'package:binge/pages/checkout.page.dart';
+import 'package:flutter/rendering.dart';
+//
+// Created by Braulio Cassule
+// 30 December 2017
+//
+void main() {
+  //debugPaintSizeEnabled=true;
+  runApp(new MyApp());
 }
-class LogoApp extends StatefulWidget {
+
+
+
+class MyApp extends StatefulWidget {
   _LogoAppState createState() => _LogoAppState();
 }
 
-class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+class _LogoAppState extends State<MyApp> with
+  SingleTickerProviderStateMixin{
+  int _page = 0;
+  Color gradientEnd = const Color(0xFFFF5640); //Change start gradient color here
+  Color gradientStart = const Color(0xFFE8E8E8);//Change end gradient color here
+  AnimationController _controller;
+  PageController _pageController;
 
-  AnimationController controller;
-
-  void handleTimeout() {
-    print('intimeout1');
-    Navigator.of(context).pushReplacementNamed('/HomeScreen');
-  }
-
-  startTime() async { 
-    var _duration = new Duration(seconds: 2); 
-    return new Timer(_duration, handleTimeout); 
-  }
-   @override
+  @override
   initState() {
-    super.initState();
-    controller = AnimationController(
-        duration: const Duration(milliseconds: 500), vsync: this);
-        controller.addListener(() {
-        setState(() {
-          // the state that has changed here is the animation object’s value
-        });
+    SystemChrome.setEnabledSystemUIOverlays([]);
+     _pageController = new PageController();
+    _controller = new AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      vsync: this,
+    );
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      this._page = page;
     });
-    controller.forward();
-    //Timer(Duration(seconds: 3), () => handleTimeout);
   }
 
   @override
   Widget build(BuildContext context) {
-    return SplashScreen(
-      controller: controller
+    TextStyle style = new TextStyle(
+        color: Color(0xFF342D2A),
+        fontFamily: 'KalamRegular',
+        fontWeight: FontWeight.normal,
+        fontSize: 20.0,
+    );
+    return new MaterialApp(
+      home: new Scaffold(
+        backgroundColor: gradientStart,
+        body: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            _topAppBar(context),
+            new Container(
+              padding: const EdgeInsets.only(top:10.0),
+            ),
+            Expanded(
+              flex: 2,
+              child: new PageView(
+                physics:new NeverScrollableScrollPhysics(),
+                children: <Widget>[
+                  MainSearchPage(
+                    controller: _controller,
+                  ),
+                  MainSearchPage(
+                    controller: _controller,
+                  ),
+                  CheckoutPage(
+                    controller: _controller,
+                  ),
+                ],
+                onPageChanged: onPageChanged,
+                controller: _pageController,
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home',style: style,)),
+            BottomNavigationBarItem(icon: Icon(Icons.shopping_basket), title: Text('Checkout',style: style,)),
+            BottomNavigationBarItem(icon: Icon(Icons.more), title: Text('More',style: style,)),
+          ],
+          fixedColor: Color(0xFF008761),
+          onTap: navigationTapped,
+          currentIndex: _page,
+        ),
+      ),
     );
   }
+  void navigationTapped(int page) {
+    // Animating to the page.
+    // You can use whatever duration and curve you like
+    _pageController.animateToPage(page,
+        duration: const Duration(milliseconds: 300), curve: Curves.ease);
+  }
 
-
-  dispose() {
-    controller.dispose();
-    super.dispose();
+  Widget _topAppBar(BuildContext context) {
+    return new Card( 
+      elevation: 3,
+      child: Container(
+    
+    height: 140,
+    decoration: BoxDecoration(
+      color: Colors.white,
+    ),
+    padding: const EdgeInsets.all(10.0),
+      child: new Column(
+        children: <Widget>[
+          new Row(
+            children: <Widget>[
+              Expanded(
+              child: new Text('Binge', style: TextStyle(
+                fontSize: 36,
+                fontFamily: 'Pacifico',
+                color: Color(0xFF008761),
+                ),
+              ),
+              ),
+              RaisedButton(
+              child:Icon(
+                Icons.search,
+                color: Color(0xFF008761),
+                size: 30.0,
+              ),
+                        onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondScreen()),
+            );
+          },
+              ),
+            ],
+          ),
+        ],
+      ),
+      ), 
+    );
   }
 }
 
-void main() {
-  runApp(
-    new MaterialApp( 
-      home: LogoApp(),
-      theme: ThemeData.dark(),
-      routes: <String, WidgetBuilder>{
-      '/HomeScreen': (BuildContext context) => new MyApp()
-    },
-    )
-  );
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
 }
